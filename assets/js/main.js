850bc0c1d291766f43df33981cf3a12b725150b6
God'sFavour Chukwudi <127259967+GChukwudi@users.noreply.github.com>
2024-03-06 18:46:24 +0200

    updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,4 +33,9 @@ const scrollActive = () => {
     })
 }
 /*=============== SCROLL REVEAL ANIMATION ===============*/
-
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+})
